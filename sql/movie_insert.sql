50e731bd006dc9c173d928b87d7ab68f2e40dc68
Zhao-Yinjie <68451635+Zhao-Yinjie@users.noreply.github.com>
2022-10-20 13:00:46 +0800

    Add files via upload

---
+++ sql/movie_insert.sql
@@ -0,0 +1,109 @@
+insert into movie (
+    title, poster_dir, details, synopsis, timeslot_index, price
+) 
+
+values 
+
+(
+    'Halloween Ends', 
+    'img_src/halloween_ends.webp', 
+    '{
+        "Cast":"Jamie Lee Curtis, Kyle Richards, Judy Greer",
+        "Director":"David Gordon Green",
+        "Genre":"Horror/Thriller",
+        "Release":"13.10.2022",
+        "Running Time":"1h 45min",
+        "Distributor":"UNI",
+        "Language":"English(Sub: Chinese)"
+    }', 
+    'Laurie Strode prepares for a final showdown with masked killer Michael Myers.', 
+    '["D1T1", "D1T2", "D2T3", "D2T4", "D3T1", "D3T2", "D4T2", "D4T3"]',
+    12.99
+),
+
+(
+    'Doctor G',
+    'img_src/doctor_g.webp',
+    '{
+        "Cast":"Ayushmann Khuranna, Rakul Preet Singh, Shefali Shah",
+        "Director":"Anubhuti Kashyap",
+        "Genre":"Comedy/Drama",
+        "Release":"14-10-2022",
+        "Running Time":"122 minutes",
+        "Distributor":"HSE",
+        "Language":"Hindi(Sub: English)"
+    }',
+    'A medical campus comedy-drama, Doctor G is about the hilarious struggles of Dr. Uday Gupta, who wanted to specialise in Orthopaedics, but is stuck in an all-female class of Gynaecology. Will he change his department, or will the department change him?',
+    '["D1T3", "D1T4", "D2T1", "D2T2", "D3T2", "D3T3", "D4T1", "D4T4"]',
+    13.99
+),
+
+(
+    'Ticket to Paradise',
+    'img_src/ticket_to_paradise.webp',
+    '{
+        "Cast":"Dwayne Johnson, Emily Blunt, Edgar Ramirez",
+        "Director":"Rawson Marshall Thurber",
+        "Genre":"Comedy/Adventure",
+        "Release":"14-10-2022",
+        "Running Time":"1h 45min",
+        "Distributor":"HSE",
+        "Language":"English(Sub: Chinese)"
+    }',
+    'A group of misfits encounter a young Yeti named Everest, and they set off to reunite the magical creature with his family on the mountain of his namesake.',
+    '["D1T1", "D1T2", "D2T3", "D2T4", "D3T4", "D4T3", "D4T4"]',
+    14.99
+),
+
+(
+    'Wandering',
+    'img_src/wandering.webp',
+    '{
+        "Cast":"Chen Hanwei, Zhang Ziyi, Wang Xuebing",
+        "Director":"Chen Hanwei",
+        "Genre":"Drama",
+        "Release":"14-10-2022",
+        "Running Time":"1h 45min",
+        "Distributor":"HSE",
+        "Language":"Chinese(Sub: English)"
+    }',
+    "In a park on a rainy evening, a 19-year-old university student, Fumi, offers an umbrella to a soaking wet 10-year-old girl, Sarasa. Realizing her reluctance to go home, Fumi lets her stay in his place, where she spends the next two months in peace. ",
+    '["D1T3", "D1T4", "D2T1", "D2T2", "D3T1", "D3T4", "D4T4"]',
+    14.99
+),
+
+(
+    'Amsterdam',
+    'img_src/amsterdam.webp',
+    '{
+        "Cast":"Dwayne Johnson, Emily Blunt, Edgar Ramirez",
+        "Director":"Rawson Marshall Thurber",
+        "Genre":"Comedy/Adventure",
+        "Release":"14-10-2022",
+        "Running Time":"1h 45min",
+        "Distributor":"HSE",
+        "Language":"English(Sub: Chinese)"
+    }',
+    "Arya never expected a man changed her life with unexpected connection.",
+    '["D1T3", "D1T4", "D2T1", "D2T2", "D3T2", "D3T3", "D4T1", "D4T4"]',
+    13.99
+),
+
+(
+    'The Legend of Maula Jatt',
+    'img_src/the_legend_of_maula_jatt.webp',
+    '{
+        "Cast":"Dwayne Johnson, Emily Blunt, Edgar Ramirez",
+        "Director":"Rawson Marshall Thurber",
+        "Genre":"Comedy/Adventure",
+        "Release":"14-10-2022",
+        "Running Time":"1h 45min",
+        "Distributor":"HSE",
+        "Language":"English(Sub: Chinese)"
+    }',
+    "In a park on a rainy evening, a 19-year-old university student, Fumi, offers an umbrella to a soaking wet 10-year-old girl, Sarasa. Realizing her reluctance to go home, Fumi lets her stay in his place, where she spends the next two months in peace. ",
+    '["D1T1", "D1T2", "D2T3", "D2T4", "D3T2", "D3T3", "D4T1", "D4T4"]',
+    12.99
+);
+
+
